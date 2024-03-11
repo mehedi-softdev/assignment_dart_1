3bffa4c15020512dbb37053bc2edaa8637df0653
mehedi-softdev <mehedi.softdev@gmail.com>
2024-03-11 10:40:44 +0600

    making attribute private

--- lib/song.dart
+++ lib/song.dart
@@ -2,12 +2,12 @@ import 'package:assignment_dart_1/media.dart';
 
 class Song extends Media {
   // attribute called "artist"
-  String artist;
+  final String _artist;
   // constructor
-  Song(this.artist);
+  Song(this._artist);
 
   @override
   void play() {
-      print("Playing song by $artist...");
+      print("Playing song by $_artist...");
   }
 }
